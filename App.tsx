75457b10fa12f1d96b72e20068572faffc68c815
Guilherme Spanserki <guilhermespanserkiohc@hotmail.com>
2023-05-17 15:04:32 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,24 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text,
+  ImageBackground,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts
+} from '@expo-google-fonts/roboto';
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+import blurImg from './assets/luz.png'
+import BgLogo from './assets/bg-logo.svg'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+  if (!hasLoadedFonts) {
+    return null
+  }
   return (
-    <View className='
-      bg-gray-950
+    <ImageBackground
+      source={blurImg}
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+      className='
+      bg-gray-900
       flex-1
       items-center
-      justify-center
+      relative
+      px-8
+      py-10
      '
     >
-      <Text className='
-        text-zinc-50
-        font-bold
-        text-3xl
+      <View
+        className='
+        flex-1
+        items-center
+        justify-center
+        gap-6
       '
       >
-        Sua Cápsula do tempo
+        <BgLogo />
+        <View
+          className='
+          space-y-2
+          '
+        >
+          <Text
+            className='
+            text-center
+            font-title
+            text-2xl
+            text-gray-50
+            leading-tight
+          '
+          >
+            Sua cápsula do tempo
+          </Text>
+          <Text
+            className='
+            text-center
+            font-body
+            text-base
+            leading-relaxed
+            text-gray-100
+          '
+          >
+            Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          className='
+          rounded-full
+          bg-green-500
+          px-5
+          py-2
+        '
+        >
+          <Text
+            className='
+            font-alt
+            text-sm
+            uppercase
+          '
+          >
+            Começar a cadastrar
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text
+        className='
+        text-center
+        font-body
+        text-sm
+        leading-relaxed
+        text-gray-200
+      '
+      >
+        Developed by SS digital agency 🚀
       </Text>
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
